5e9e0b95a058628cb1c62aa04b55f42a91072bcc
finch185277 <59487andy@gmail.com>
2019-11-28 17:20:52 +0800

    Update test.sh

--- hw3/test.sh
+++ hw3/test.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 
 input_folder="input"
+error_folder="error"
 output_folder="output"
-no_error_folder="no-parsing-error"
 
-input_error_files="test-minus-neg.p test-parser-error.p"
-input_no_error_files=$(ls ${input_folder}/${no_error_folder})
+input_files=$(ls ${input_folder} | grep -v ${error_folder})
+input_error_files=$(ls ${input_folder}/${error_folder})
 
-if [ ! -d ${output_folder}/${no_error_folder} ] || [ ! -d ${output_folder}/${no_error_folder} ]; then
+if [ ! -d ${output_folder} ]; then
   mkdir -p ${output_folder}
-  mkdir -p ${output_folder}/${no_error_folder}
 fi
+
+if [ ! -d ${output_folder}/${error_folder} ]; then
+  mkdir -p ${output_folder}/${error_folder}
+fi
+
+for file in ${input_files}; do
+  echo "--------------- [${file}] ---------------"
+  # ./parser < ${input_folder}/"${file%.*}".p > ${output_folder}/"${file%.*}".txt
+  touch ${output_folder}/"${file%.*}".txt
+done
 
 for file in ${input_error_files}; do
   echo "--------------- [${file}] ---------------"
-  ./parser < ${input_folder}/"${file%.*}".p > ${output_folder}/"${file%.*}".txt
-done
-
-for file in ${input_no_error_files}; do
-  echo "--------------- [${file}] ---------------"
-  ./parser < ${input_folder}/${no_error_folder}/"${file%.*}".p > ${output_folder}/${no_error_folder}/"${file%.*}".txt
+  # ./parser < ${input_folder}/${no_error_folder}/"${file%.*}".p > ${output_folder}/${no_error_folder}/"${file%.*}".txt
+  touch ${output_folder}/${error_folder}/"${file%.*}".txt
 done
