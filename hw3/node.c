28e2abbccc8f9e0bab96d4b3a4ef9e727817fb09
finch185277 <59487andy@gmail.com>
2019-11-29 00:23:33 +0800

    Update node

--- hw3/node.c
+++ hw3/node.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node *new_node(char *str) {
+struct Node *new_node(int type) {
   struct Node *node = (struct Node *)malloc(sizeof(struct Node));
-  node->name = str;
+  node->node_type = type;
+  node->node_str = NULL;
   node->parent = NULL;
   node->child = NULL;
   node->lsibling = node;
@@ -13,8 +14,9 @@ struct Node *new_node(char *str) {
 }
 
 void delete_node(struct Node *node) {
-  if (node != NULL)
-    free(node);
+  if (node->node_str != NULL)
+    free(node->node_str);
+  free(node);
 }
 
 void add_child(struct Node *node, struct Node *child) {
@@ -32,8 +34,37 @@ void add_child(struct Node *node, struct Node *child) {
 void print_tree(struct Node *node, int ident) {
   for (int i = 0; i < ident; i++)
     printf(" ");
-  printf("%s\n", node->name);
+
+  switch (node->node_type) {
+  case TOKEN_INTEGER:
+    printf("%d\n", node->integer_value);
+  case TOKEN_REAL:
+    printf("%f\n", node->real_value);
+  case TOKEN_STRING:
+    printf("%s\n", node->node_str);
+  case TOKEN_PLUS:
+    printf("+\n");
+  case TOKEN_MINUS:
+    printf("-\n");
+  case TOKEN_STAR:
+    printf("*\n");
+  case TOKEN_SLASH:
+    printf("/\n");
+  case TOKEN_GT:
+    printf(">\n");
+  case TOKEN_GE:
+    printf(">=\n");
+  case TOKEN_LT:
+    printf("<\n");
+  case TOKEN_LE:
+    printf("<=\n");
+  case TOKEN_EQUAL:
+    printf("=\n");
+  case TOKEN_notEQUAL:
+    printf("!=\n");
+  }
   ident++;
+
   struct Node *child = node->child;
   if (child != NULL) {
     do {
